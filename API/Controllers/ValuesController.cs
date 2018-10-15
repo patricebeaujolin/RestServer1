520a8d48c07cbcb2c48d881cb10db5db00ee3b07
patricebeaujolin <patrice.beaujolin@gmail.com>
2018-10-15 13:27:07 +0200

    - moved and renamed API main project

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Logger.Controllers
+namespace RestServer1.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
